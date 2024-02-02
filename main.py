83d1055ae56a5d77fa6e6bd0ccf946f66426a162
Brian Mann <brianpatrickmann@gmail.com>
2024-02-01 19:40:09 -0500

    remove unneeded steps in main

--- main.py
+++ main.py
@@ -4,23 +4,7 @@ from utilities.matchscript import create_matches_file, update_match_results
 if __name__ == '__main__':
 
   #create_matches_file("vct-lockin-sao-paulo-2023")
-  #create_matches_file("vct-americas-league-2023")
-  #create_matches_file("vct-emea-league-2023")
-  #create_matches_file("vct-pacific-league-2023")
-  #create_matches_file("vct-masters-tokyo-2023")
-  #create_matches_file("vct-americas-last-chance-qualifier-2023")
-  #create_matches_file("vct-emea-last-chance-qualifier-2023")
-  #create_matches_file("vct-pacific-last-chance-qualifier-2023")
-  #create_matches_file("valorant-champions-2023")
 
-  update_match_results("vct-lockin-sao-paulo-2023")
-  update_match_results("vct-americas-league-2023")
-  update_match_results("vct-emea-league-2023")
-  update_match_results("vct-pacific-league-2023")
-  update_match_results("vct-masters-tokyo-2023")
-  update_match_results("vct-americas-last-chance-qualifier-2023")
-  update_match_results("vct-emea-last-chance-qualifier-2023")
-  update_match_results("vct-pacific-last-chance-qualifier-2023")
-  update_match_results("valorant-champions-2023")
+  #update_match_results("vct-lockin-sao-paulo-2023")
 
   plot_ratings_vs_wins()
